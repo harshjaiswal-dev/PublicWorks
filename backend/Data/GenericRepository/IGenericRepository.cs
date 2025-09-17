d125d7fa85882a95028af4b5f7a05037df770a2e
harshjaiswal-dev <harsh.jaiswal@agreeya.net>
2025-09-17 11:23:35 +0530

    Generic Repository

---
+++ backend/Data/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,13 @@
+namespace Data.GenericRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task SaveAsync();
+        void Update(T entity);
+        void Delete(T entity);
+        
+    }
+}
